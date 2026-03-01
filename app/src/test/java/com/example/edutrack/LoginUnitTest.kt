0bdd122ce893bceb6e0740f5be8ea85f4dec88d1
shital360 <shitaldangol@gmail.com>
2026-03-01 12:00:21 +0545

    loginunittest

---
+++ app/src/test/java/com/example/edutrack/LoginUnitTest.kt
@@ -0,0 +1,39 @@
+package com.example.edutrack
+
+
+import com.example.edutrack.repository.UserRepo
+import com.example.edutrack.viewmodel.UserViewModel
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertTrue
+import org.junit.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.eq
+import org.mockito.kotlin.doAnswer
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+class LoginUnitTest {
+    @Test
+    fun login_success_test() {
+        val repo = mock<UserRepo>()
+        val viewModel = UserViewModel(repo)
+
+        doAnswer { invocation ->
+            val callback = invocation.getArgument<(Boolean, String) -> Unit>(2)
+            callback(true, "Login success")
+            null
+        }.`when`(repo).login(eq("test@gmail.com"), eq("123456"), any())
+
+        var successResult = false
+        var messageResult = ""
+
+        viewModel.login("test@gmail.com", "123456") { success, msg ->
+            successResult = success
+            messageResult = msg
+        }
+
+        assertTrue(successResult)
+        assertEquals("Login success", messageResult)
+
+        verify(repo).login(eq("test@gmail.com"), eq("123456"), any())
+    }
+}
